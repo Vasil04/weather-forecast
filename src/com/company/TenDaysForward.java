fddb5d9adf8d4a4d9e672e584c02d5e335bba74e
Nikola Nikolaev <nikola_nikolaev1@abv.bg>
2020-05-30 00:16:47 +0300

    Created class Ten Days Forward

--- src/com/company/TenDaysForward.java
+++ src/com/company/TenDaysForward.java
@@ -1,4 +1,31 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class TenDaysForward {
+    
+    public static void readUntilSpecialSymbol(String file) throws Exception
+    {
+        String fileName = "weather.txt";
+        int counter = 0;
+
+        String line = null;
+        FileReader fileReader1 = null;
+
+        fileReader1 = new FileReader(fileName);
+        BufferedReader bufferedReader = new BufferedReader(fileReader1);
+
+        while((line = bufferedReader.readLine()) != null) {
+            counter++;
+            if(counter >= 9 && counter <=18)
+            {
+                printer(line);
+                System.out.println("-------------------------------------------------------------------------------");
+            }
+        }
+    }
+
+    private static void printer(String line) {
+    }
 }
