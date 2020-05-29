402bba9d0f40f6aa1bd23d104d9f89b18c23f2be
Vasil <vdvasilev04@gmail.com>
2020-05-30 01:55:14 +0300

    Almost done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,18 +8,15 @@ import java.nio.file.*;;
 public class Main {
 
     public static void main(String[] args) {
+        String fileName = "weather.txt";
         try {
-            DefaultReader.readUntilSpecialSymbol("weather.txt");
+            DefaultReader.readUntilSpecialSymbol(fileName);
         } catch (Exception e) {
             e.printStackTrace();
         }
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Изберете опция:");
-        System.out.println("За по-подробна информация за времето утре - '1'");
-        System.out.println("За прогнозата за 10 дни напред - '2'");
-        System.out.println("За прогнозата за 10 дни назад - '3'");
-        System.out.println("За да прекратите програмата - 'стоп' или 'stop'");
+        options();
         String choice = "";
         while (true){
             choice = input.nextLine();
@@ -28,21 +25,38 @@ public class Main {
             }
             else if (choice.equalsIgnoreCase("1")){
                 try {
-                    Tomorrow.readUntilSpecialSymbol("weather.txt");
+                    Tomorrow.readUntilSpecialSymbol(fileName);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
             else if (choice.equalsIgnoreCase("2")){
-
+                try {
+                    TenDaysForward.readUntilSpecialSymbol(fileName);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
             else if (choice.equalsIgnoreCase("3")){
-
+                try {
+                    TenDaysBefore.readUntilSpecialSymbol(fileName);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
             else {
                 System.out.println("Избраната от вас опция е невалидна");
             }
+            options();
         }
+    }
+
+    public static void options() {
+        System.out.println("Изберете опция:");
+        System.out.println("За по-подробна информация за времето утре - '1'");
+        System.out.println("За прогнозата за 10 дни напред - '2'");
+        System.out.println("За прогнозата за 10 дни назад - '3'");
+        System.out.println("За да прекратите програмата - 'стоп' или 'stop'");
     }
 }
